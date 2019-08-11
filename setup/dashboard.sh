3ba086b4d43139ac4de9d74128816596bea65401
Christian Köhler <ckoehler99@gmail.com>
2019-08-11 13:07:26 +0200

    Update dashboard.sh

--- setup/dashboard.sh
+++ setup/dashboard.sh
@@ -1,2 +1,14 @@
 #bash 
+
+# deploy dashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+
+# user and token for dashboard login
+kubectl create serviceaccount dashboard-admin-sa
+kubectl create clusterrolebinding dashboard-admin-sa --clusterrole=cluster-admin --serviceaccount=default:dashboard-admin-sa
+kubectl describe $(kubectl get secrets -o name | grep dashboard-admin-sa-token)
+
+# start proxy 
+kubectl proxy
+
+# open http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/
